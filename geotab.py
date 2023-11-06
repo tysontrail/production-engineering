2c2006b8880513f96d055ba8c797d660955f971c
ttrail <tyson.trail@gmail.com>
2023-11-06 16:43:31 -0700

    Complete transformations on trips and drivers tables

--- geotab.py
+++ geotab.py
@@ -1,5 +1,6 @@
 import mygeotab
 import pandas as pd
+import json
 
 # -----------------
 # Helper Function
@@ -43,27 +44,74 @@ api.authenticate()
 
 # Get all devices, trips, and drivers
 # devices = api.get("Device", resultsLimit=1)
-# trips = api.get("Trip", resultsLimit=1)
-# drivers = api.get("User", resultsLimit=1)
+trips = api.get("Trip", resultsLimit=100, fromDate="2023-10-10", toDate="2023-10-12")
+drivers = api.get("User", resultsLimit=100)
 groups = api.get("Group", resultsLimit=100)
 
 # Convert to pandas dataframes
 # devices = pd.DataFrame(devices)
-# trips = pd.DataFrame(trips)
-# drivers = pd.DataFrame(drivers)
+trips = pd.DataFrame(trips)
+drivers = pd.DataFrame(drivers)
+print(drivers.head())
 groups = pd.DataFrame(groups)
+
+
+# Extract the 'id' from the 'Driver' column
+def get_driver_id(driver_info):
+    # Check if driver_info is a dictionary and has an 'id' key
+    if isinstance(driver_info, dict):
+        return driver_info.get(
+            "id", None
+        )  # Get 'id' if it exists, otherwise return None
+    # Check if driver_info is a string representation of a dictionary
+    elif isinstance(driver_info, str) and driver_info.startswith("{"):
+        try:
+            # Try to parse the string as JSON
+            driver_dict = json.loads(driver_info)
+            return driver_dict.get("id", None)
+        except json.JSONDecodeError:
+            # Handle the case where the string isn't valid JSON
+            return None
+    else:
+        # If it's a string but not a dict representation, or some other type, return None or handle differently
+        return None
+
+
+# Apply the function to each element in the 'driver' column
+trips["driverid"] = trips["driver"].apply(get_driver_id)
+
+# Drop the original 'Driver' column
+trips = trips.drop("driver", axis=1)
+
+# Transform drivers
+# Explode the driverGroups column
+drivers = drivers.explode("driverGroups")
+
+# Extract the number from the dictionaries in 'driverGroups'
+drivers["driverGroupIDs"] = drivers["driverGroups"].apply(
+    lambda x: x["id"] if isinstance(x, dict) and "id" in x else x
+)
+
+# Drop the original 'driverGroups' column
+drivers = drivers.drop("driverGroups", axis=1)
+
+# Export to CSV
+# devices.to_csv("devices.csv")
+trips.to_csv("trips.csv")
+drivers.to_csv("drivers.csv")
+# groups.to_csv("groups.csv")
 
 # Print column names
 # print(f"Device Columns: {devices.columns}")
 # print(f"Trip Columns: {trips.columns}")
 # print(f"Driver Columns: {drivers.columns}")
-print(f"Group Columns: {groups.columns}")
+# print(f"Group Columns: {groups.columns}")
 
 # Adjust display options
-pd.set_option("display.max_rows", None)
-pd.set_option("display.max_columns", None)
+# pd.set_option("display.max_rows", None)
+# pd.set_option("display.max_columns", None)
 
-print(groups)
+# print(drivers.head(10))
 
 # print(f"Driver Columns: {drivers.columns}")
 
